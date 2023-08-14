dee06e8fc911ce5c65ef7fbede27da34a5a6a035
XK <kaitanidis.xristos@gmail.com>
2023-08-14 12:51:03 +0300

    updates

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll';
 //import { robots } from './robots';
-import Searchbox from './Searchbox';
+import Searchbox from '../components/Searchbox';
 import './App.css';
 
 
@@ -26,23 +27,22 @@ class App extends Component {
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if(this.state.robots.length === 0){
-            return <h1>Loading</h1>
-        } else {
-        return (
-                <div className='tc'>
-                    <h1 className='f1'>Robofriends</h1>
-                    <Searchbox searchChange={this.OnSearchChange}/>
+        return !robots.length ?
+        <h1>Loading</h1> :
+        (
+            <div className='tc'>
+                <h1 className='f1'>Robofriends</h1>
+                <Searchbox searchChange={this.OnSearchChange}/>
+                <Scroll>
                     <CardList robots={filteredRobots}/>
-                </div>
-            );
-        }
-
+                </Scroll> 
+            </div>
+        );
     }
-
 }
 
 export default App;
